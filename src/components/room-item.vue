62f0bd5dc99a2e449246d0a89766daa77206683b
GubarSerg <gubar.serg@gmail.com>
2019-04-09 12:01:07 +0300

    fixed

---
+++ src/components/room-item.vue
@@ -0,0 +1,49 @@
+<template>
+  <md-list-item>
+    <slot></slot>
+    <div v-if="showActionButtons" class="action-buttons">
+      <md-button class="md-icon-button md-list-action" @click="onEditClick">
+        <md-icon>edit</md-icon>
+      </md-button>
+      <md-button class="md-icon-button md-list-action" @click="onDeleteClick">
+        <md-icon>delete</md-icon>
+      </md-button>
+    </div>
+  </md-list-item>
+</template>
+
+<script>
+export default {
+  name: 'room-item',
+  props: {
+    showActionButtons: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    onEditClick: {
+      type: Function,
+      required: false,
+      default: () => {},
+    },
+    onDeleteClick: {
+      type: Function,
+      required: false,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.action-buttons {
+  display: none;
+}
+.md-list-item {
+  &:hover {
+    .action-buttons {
+      display: block;
+    }
+  }
+}
+</style>
