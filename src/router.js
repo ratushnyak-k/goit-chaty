6d37c256904e04ed17907617e3e94da5678b2dd4
GubarSerg <gubar.serg@gmail.com>
2019-04-10 23:05:41 +0300

    add rooms-list

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
+import Rooms from './views/rooms-list.vue';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About,
+    },
+    {
+      path: '/rooms',
+      name: 'rooms',
+      component: Rooms,
     },
   ],
 });
---
+++ src/views/rooms-list.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="fixed">
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">ROOM</span>
+      </md-app-toolbar>
+
+      <md-app-drawer md-permanent="full">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          Rooms
+          <md-button class="md-icon-button">
+            <md-icon>add</md-icon>
+          </md-button>
+        </md-toolbar>
+        <md-divider></md-divider>
+
+        <md-list>
+          <room-item v-for="room in rooms" v-bind:key="room.id" v-bind:name="room.name">{{
+            room.name
+          }}</room-item>
+        </md-list>
+        <md-button class="md-raised md-accent">Logout</md-button>
+      </md-app-drawer>
+
+      <md-app-content> </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.md-app {
+  max-height: 400px;
+  border: 1px solid rgba(#000, 0.12);
+}
+
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
+.md-app-drawer {
+  display: flex;
+  flex-direction: column;
+}
+.md-raised {
+  margin-top: auto;
+}
+</style>
+
+<script>
+import roomItem from '@/components/room-item.vue';
+
+export default {
+  name: 'rooms-list',
+  components: {
+    roomItem,
+  },
+  data: function() {
+    return {
+      rooms: [
+        { id: 1, name: 'Room name 1', creatorId: 1 },
+        { id: 2, name: 'Room name 2', creatorId: 2 },
+        { id: 3, name: 'Room name 3', creatorId: 3 },
+      ],
+    };
+  },
+};
+</script>
