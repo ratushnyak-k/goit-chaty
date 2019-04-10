d7f27d01076f6ef63b2eaf9bc9edbe04c0648e4d
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-04-10 23:04:09 +0300

    chat-input

--- src/component/chat-input.vue
+++ src/component/chat-input.vue
@@ -1,35 +1,33 @@
 <template>
-  <form>
-    <label for="text_message">Type message...</label>
-    <input name="text_message" type="text" v-model="value">
+  <form @submit.prevent="addMessage">
+    <md-field>
+      <label for="text_message">Type message...</label>
+      <md-input name="text_message" type="text" v-model="value" />
+    </md-field>
   </form>
 </template>
 
 <script>
 export default {
   name: 'chat-input',
-  props: { onSubmit: { type: Function } },
-  data: function() {
+  props: {
+    onSubmit: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
     return { value: '' };
   },
 
   methods: {
     addMessage() {
       const newMessage = this.value.trim();
-      if (newMessage != 0) {
-        return this.onSubmit({ value: this.value });
-        this.value = '';
+      if (newMessage !== 0) {
+        this.onSubmit({ value: newMessage });
       }
+      this.value = '';
     },
   },
 };
 </script>
-
-
-<style lang="scss" scoped>
-.input {
-  width: 100%;
-  padding: 8px 10px;
-  border: 1px solid $vue-blue;
-}
-</style>
