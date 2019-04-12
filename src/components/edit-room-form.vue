c1987c3e000490a5aebf57236c52b1d60e1e8fc8
SoftPro <support@softpro.ua>
2019-04-12 18:20:20 +0300

    fixed bugs

--- src/components/edit-room-form.vue
+++ src/components/edit-room-form.vue
@@ -1,126 +1,105 @@
 <template>
-  <div>
-    <form novalidate class="md-layout" @submit.prevent="validateRoom">
-      <md-card class="md-layout-item md-size-50 md-small-size-100">
-        <md-card-header>
-          <div class="md-title" v-if="name">Edit Room</div>
-          <div class="md-title" v-if="!name">Add Room</div>
-        </md-card-header>
+  <form novalidate class="md-layout" @submit.prevent="validateRoom">
+    <md-card class="md-layout-item">
+      <md-card-header>
+        <div class="md-title">{{ name ? `Edit ${name}` : 'Add room' }}</div>
+      </md-card-header>
 
-        <md-card-content>
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('room')">
-                <label for="room-name">Room Name </label>
+      <md-card-content>
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item">
+            <md-field :class="getValidationClass('room')">
+              <label for="room-name">Room Name</label>
 
-                <md-input
-                  name="room-name"
-                  id="room-name"
-                  autocomplete="given-name"
-                  v-model="form.room"
-                  :disabled="sending"
-                />
-                <span class="md-error" v-if="!$v.form.room.required"
-                  >The Room name is required</span
-                >
-                <span class="md-error" v-else-if="!$v.form.room.minlength">Invalid room name</span>
-              </md-field>
-            </div>
+              <md-input
+                name="room-name"
+                id="room-name"
+                autocomplete="given-name"
+                v-model="form.room"
+                :disabled="sending"
+              />
+            </md-field>
           </div>
-        </md-card-content>
+        </div>
+      </md-card-content>
 
-        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+      <md-progress-bar md-mode="indeterminate" v-if="sending"/>
 
-        <md-card-actions>
-          <md-button type="submit" class="md-primary" :disabled="sending" v-if="name"
-            >Edit Room</md-button
-          >
-          <md-button type="submit" class="md-primary" :disabled="sending" v-if="!name"
-            >Add Room</md-button
-          >
-        </md-card-actions>
-      </md-card>
+      <md-card-actions>
+        <md-button
+          type="submit"
+          class="md-primary"
+          :disabled="sending"
+        >{{ name ? 'Edit' : 'Add' }} Room</md-button>
+      </md-card-actions>
+    </md-card>
 
-      <md-snackbar :md-active.sync="roomSaved"
-        >The room {{ lastRoom }} was saved with success!</md-snackbar
-      >
-    </form>
-  </div>
+    <md-snackbar :md-active.sync="roomSaved">The room {{ lastRoom }} was saved with success!</md-snackbar>
+  </form>
 </template>
 
 <script>
-import { validationMixin } from 'vuelidate';
-import { required, minLength } from 'vuelidate/lib/validators';
-
+  import { validationMixin } from 'vuelidate';
+  import { required, minLength } from 'vuelidate/lib/validators';
 export default {
   name: 'FormValidation',
   mixins: [validationMixin],
   props: {
-    name: { type: String },
+   name: { 
+      type: String,
+      required: false,
+      default: '',
+    },
+    sending: false,
     onSubmit: {
       type: Function,
       require: true,
     },
   },
-
-  data: function() {
+  data() {
     return {
       form: {
         room: '',
       },
-      sending: false,
+      
       lastRoom: '',
       roomSaved: false,
-    };
-  },
-  mounted: function() {
-    this.form.room = this.name; /*this.$route.params.name||''*/
+    }
   },
   validations: {
     form: {
       room: {
         required,
-        normalizer: function(value) {
-          this.value = (value || '').toString().trim();
+        normalizer(value) {
+          let str= value.toString();
+          this.value =str.trim();
           return this.value;
-        },
-        minLength: minLength(3),
+        }
       },
     },
   },
   methods: {
     getValidationClass(fieldName) {
       const field = this.$v.form[fieldName];
-
       if (field) {
         return {
           'md-invalid': field.$invalid && field.$dirty,
         };
       }
     },
-
     clearForm() {
       this.$v.$reset();
       this.form.room = null;
     },
-
     editRoom() {
-      this.sending = true;
-      this.lastRoom = this.form.room;
-
-      /* === Instead of this timeout, here you can call your API === */
-
-      window.setTimeout(() => {
-        this.sending = false;
-        this.roomSaved = true;
-        this.onSubmit({ value: this.lastRoom });
-        this.clearForm();
-      }, 1500);
+    //  this.sending = true;
+      this.lastRoom = this.form.room;      
+      this.onSubmit({ name: this.lastRoom });
+      this.clearForm();
+      this.roomSaved = true;
     },
-
-    validateRoom() {
+     validateRoom() {
       this.$v.$touch();
-
       if (!this.$v.$invalid) {
         this.editRoom();
       }
