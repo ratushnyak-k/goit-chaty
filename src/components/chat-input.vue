9dd1e79ede18caad23d8b918760f4ea1332e853f
SvetlanaVoshchevskaya <39382391+SvetlanaVoshchevskaya@users.noreply.github.com>
2019-04-12 21:17:36 +0300

    Implement chat-input component (#15)
    * 'deleteComment'

---
+++ src/components/chat-input.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="addMessage">
+    <md-field>
+      <label for="text_message">Type message...</label>
+      <md-input name="text_message" type="text" v-model.trim="value" />
+    </md-field>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'chat-input',
+  props: {
+    onSubmit: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return { value: '' };
+  },
+
+  methods: {
+    addMessage() {
+      if (this.value) {
+        this.onSubmit({ value: this.value });
+        this.value = '';
+      }
+    },
+  },
+};
+</script>
