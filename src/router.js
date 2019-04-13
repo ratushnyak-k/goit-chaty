02e0c9dc279ce276ecb15bfbd279131a3300f8aa
konstantin.ratushniak <konstantin.ratushniak@yola.com>
2019-04-13 12:42:39 +0300

    get rid of unused files

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
+import Home from './views/home.vue';
 import RoomsList from './views/rooms-list.vue';
 
 Vue.use(Router);
@@ -14,11 +13,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
     },
     {
       path: '/rooms',
