b9f978133a92dbe04782e5eaed7d8d66e15f3f84
Yevhenii Fedko <lu13697@gmail.com>
2019-04-13 17:58:45 +0300

    add sign-in form component

---
+++ src/components/sign-in-form.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <form novalidate class="md-layout" @submit.prevent="validateUser">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+      
+        <md-card-content>
+          <md-field :class="getValidationClass('email')">
+            <label for="email">Email</label>
+            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.email.required">The field is required</span>
+            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+          </md-field>
+          <md-field :class="getValidationClass('password')">
+            <label for="password">Password</label>
+            <md-input type="password" name="password" id="password" autocomplete="password" v-model="form.password" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.password.required">The field is required</span>
+          </md-field>
+        </md-card-content>
+
+        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+        <md-card-actions>
+          <md-button type="submit" class="md-raised md-primary" :disabled="sending">Submit</md-button>
+        </md-card-actions>
+      </md-card>
+
+      <md-snackbar :md-active.sync="userSaved">Sign in was success!</md-snackbar>
+    </form>
+  </div>
+</template>
+
+<script>
+  import { validationMixin } from 'vuelidate'
+  import {
+    required,
+    email,
+  } from 'vuelidate/lib/validators'
+
+  export default {
+    name: 'sign-in-form',
+    mixins: [validationMixin],
+    props:{
+      onSubmit:{
+        type: Function,
+        required: true,
+      },
+      sending:{
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+    },
+    data: () => ({
+      form: {
+        email: '',
+        password: '',
+      },
+      userSaved: false,
+      lastUser: null
+    }),
+    validations: {
+      form: {
+        password: {
+          required,
+        },
+        email: {
+          required,
+        }
+      }
+    },
+    methods: {
+      getValidationClass (fieldName) {
+        const field = this.$v.form[fieldName]
+
+        if (field) {
+          return {
+            'md-invalid': field.$invalid && field.$dirty
+          }
+        }
+      },
+      clearForm () {
+        this.$v.$reset()
+        this.form.password = null
+        this.form.email = null
+      },
+      
+      validateUser () {
+        this.$v.$touch()
+
+        if (!this.$v.$invalid) {
+          this.onSubmit({...this.form})
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .md-progress-bar {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+  }
+  .md-card {
+    justify-content: center;
+  }
+</style>
