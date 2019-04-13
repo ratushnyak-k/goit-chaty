bef08b60accb6ea2b9f42e096a030303dae04799
GubarSerg <gubar.serg@gmail.com>
2019-04-13 12:06:56 +0300

    added room-detail

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
 import RoomsList from './views/rooms-list.vue';
+import RoomDetail from './views/room-detail.vue';
 
 Vue.use(Router);
 
@@ -24,6 +25,11 @@ export default new Router({
       path: '/rooms',
       name: 'rooms',
       component: RoomsList,
+    },
+    {
+      path: '/room-detail',
+      name: 'room-detail',
+      component: RoomDetail,
     },
   ],
 });
---
+++ src/views/room-detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p v-if="messages.length === 0">No messages yet</p>
+    <div class="message" v-for="message in messages" :key="message.createdAt">
+      {{ message.value }}
+    </div>
+    <chat-input :onSubmit="onSubmit"></chat-input>
+  </div>
+</template>
+
+<script>
+import ChatInput from '@/components/chat-input.vue';
+
+export default {
+  name: 'room-detail',
+  components: {
+    ChatInput,
+  },
+  data() {
+    return {
+      messages: [],
+    };
+  },
+  methods: {
+    onSubmit({ value }) {
+      this.messages.push({ value, createdAt: +new Date() });
+    },
+  },
+};
+</script>
