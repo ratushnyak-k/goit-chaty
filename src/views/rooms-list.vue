9bdeeb72f5ee361db98ccd976e7ddb4af28e4171
Yulia <nayanzina@gmail.com>
2019-04-13 13:24:51 +0300

    add edit del buttons

--- src/views/rooms-list.vue
+++ src/views/rooms-list.vue
@@ -7,20 +7,45 @@
 
       <md-app-drawer md-permanent="full">
         <md-toolbar class="md-transparent" md-elevation="0">
-          Rooms
-          <md-button class="md-icon-button">
+          Rooms 
+          <md-button class="md-icon-button" @click="onAddRoom">
             <md-icon>add</md-icon>
           </md-button>
         </md-toolbar>
         <md-divider></md-divider>
 
         <md-list>
-          <room-item v-for="room in rooms" :key="room.id">{{ room.name }}</room-item>
+          <room-item v-for="room in rooms" :key="room.id">{{ room.name }} 
+            <md-button class="md-icon-button" @click="onEditRoom">
+              <md-icon>edit</md-icon>
+            </md-button>
+            <md-button class="md-icon-button" @click="showDialogType = 'delete'">
+              <md-icon>delete</md-icon>
+            </md-button></room-item>
         </md-list>
         <md-button class="md-raised md-accent">Logout</md-button>
       </md-app-drawer>
 
-      <md-app-content></md-app-content>
+      <md-app-content>
+        <md-dialog-confirm
+        :md-active.sync="showDeleteDialog"
+        md-title="Confirm delete Room?"
+        md-content="Confirm delete Room"
+        md-confirm-text="Delete"
+        md-cancel-text="Cancel"
+        @md-cancel="onCloseDialog"
+        @md-confirm="onDeleteRoom"
+        />
+
+        <md-dialog
+        :md-active.sync="showAddEditDialog"
+        :md-close-on-esc="true"
+        :md-click-outside-to-close="true"
+        >
+        <EditRoom :onSubmit="dialogData.onSubmit" :onClose="onCloseDialog" :name="dialogData.name"></EditRoom>
+        </md-dialog>
+        
+      </md-app-content>
     </md-app>
   </div>
 </template>
@@ -50,32 +75,78 @@
 
 <script>
 import RoomItem from '@/components/room-item.vue';
-
+import EditRoom from '@/components/edit-room-form';
 export default {
   name: 'rooms-list',
   components: {
     RoomItem,
+    EditRoom,
   },
-  data() {
-    return {
-      rooms: [
-        { id: 1, name: 'Room name 1', creatorId: 1 },
-        { id: 2, name: 'Room name 2', creatorId: 2 },
-        { id: 3, name: 'Room name 3', creatorId: 3 },
-        { id: 1, name: 'Room name 1', creatorId: 1 },
-        { id: 2, name: 'Room name 2', creatorId: 2 },
-        { id: 3, name: 'Room name 3', creatorId: 3 },
-        { id: 1, name: 'Room name 1', creatorId: 1 },
-        { id: 2, name: 'Room name 2', creatorId: 2 },
-        { id: 3, name: 'Room name 3', creatorId: 3 },
-        { id: 1, name: 'Room name 1', creatorId: 1 },
-        { id: 2, name: 'Room name 2', creatorId: 2 },
-        { id: 3, name: 'Room name 3', creatorId: 3 },
-        { id: 1, name: 'Room name 1', creatorId: 1 },
-        { id: 2, name: 'Room name 2', creatorId: 2 },
-        { id: 3, name: 'Room name 3', creatorId: 3 },
-      ],
-    };
+  data: () => ({
+
+    showDialogType: '',
+    dialogData: {},
+    rooms: [
+      { id: 1, name: 'Room name 1', creatorId: 1 },
+      { id: 2, name: 'Room name 2', creatorId: 2 },
+      { id: 3, name: 'Room name 3', creatorId: 3 },
+      { id: 1, name: 'Room name 1', creatorId: 1 },
+      { id: 2, name: 'Room name 2', creatorId: 2 },
+      { id: 3, name: 'Room name 3', creatorId: 3 },
+      { id: 1, name: 'Room name 1', creatorId: 1 },
+      { id: 2, name: 'Room name 2', creatorId: 2 },
+      { id: 3, name: 'Room name 3', creatorId: 3 },
+      { id: 1, name: 'Room name 1', creatorId: 1 },
+      { id: 2, name: 'Room name 2', creatorId: 2 },
+      { id: 3, name: 'Room name 3', creatorId: 3 },
+      { id: 1, name: 'Room name 1', creatorId: 1 },
+      { id: 2, name: 'Room name 2', creatorId: 2 },
+      { id: 3, name: 'Room name 3', creatorId: 3 },
+    ],
+  }),
+  computed: {
+    showAddEditDialog: {
+      get() {
+        return ['add', 'edit'].includes(this.showDialogType);
+      },
+      set() {},
+    },
+    showDeleteDialog: {
+      get() {
+        return this.showDialogType === 'delete';
+      },
+      set() {},
+    },
+  },
+  methods: {
+    onAddRoom() {
+      this.showDialogType = 'add';
+      this.dialogData = {
+        onSubmit: (formData) => {
+          console.log(formData);
+          this.onCloseDialog();
+        },
+        name: '',
+      };
+    },
+    onEditRoom() {
+      this.showDialogType = 'edit';
+      this.dialogData = {
+        name: 'Room name',
+        id: 1,
+        onSubmit: (formData) => {
+          console.log(formData);
+          this.onCloseDialog();
+        },
+      };
+    },
+    onDeleteRoom() {
+      this.showDialogType = '';
+    },
+    onCloseDialog() {
+      this.showDialogType = '';
+      this.dialogData = {};
+    },
   },
 };
 </script>
