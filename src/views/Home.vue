81e9d7f184e806f2c63c91ca54dfa0c5d90a7a16
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-04-13 13:38:14 +0300

    chat-item-component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,3 @@
 <template>
-  <div>
-    <div>
-      <small>Regular</small>
-      <md-button class="md-fab">
-        <md-icon>menu</md-icon>
-      </md-button>
-      <md-button class="md-fab md-primary">
-        <md-icon>add</md-icon>
-      </md-button>
-      <md-button class="md-fab md-plain">
-        <md-icon>edit</md-icon>
-      </md-button>
-    </div>
-
-    <div>
-      <small>Mini/Dense</small>
-      <md-button class="md-fab md-mini">
-        <md-icon>menu</md-icon>
-      </md-button>
-      <md-button class="md-fab md-mini md-primary">
-        <md-icon>add</md-icon>
-      </md-button>
-      <md-button class="md-fab md-mini md-plain">
-        <md-icon>edit</md-icon>
-      </md-button>
-    </div>
-  </div>
-</template>
-
-<style lang="scss" scoped>
-small {
-  display: block;
-}
-</style>
+  <h1>Here will be our description</h1>
+</template> 
