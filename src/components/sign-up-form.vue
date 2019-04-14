81861b7f547bbb22ffdfd7ea9bc57a408387e455
konstantin.ratushniak <konstantin.ratushniak@yola.com>
2019-04-14 08:23:33 +0300

    get rid of unnecessary code. add progress

--- src/components/sign-up-form.vue
+++ src/components/sign-up-form.vue
@@ -1,82 +1,90 @@
 <template>
-  <div>
-    <form novalidate @submit.prevent="validateUser">
-      <md-card class=" md-size-50 md-large-size-100">
-        <md-card-header>
-          <div class="md-display-1">Sign up</div>
-        </md-card-header>
-        <md-card-content>
-          <div class="md-small-size-50">
-            <md-field :class="getValidationClass('firstName')">
-              <label for="first-name">First Name</label>
-              <md-input
-                name="first-name"
-                id="first-name"
-                autocomplete="given-name"
-                v-model="form.firstName"
-                :disabled="sending"
-              />
-              <span class="md-error" v-if="!$v.form.firstName.required"
-                >The first name is required</span
-              >
-            </md-field>
-          </div>
-          <div class=" md-small-size-100">
-            <md-field :class="getValidationClass('lastName')">
-              <label for="last-name">last Name</label>
-              <md-input
-                name="last-name"
-                id="last-name"
-                autocomplete="family-name"
-                v-model="form.lastName"
-                :disabled="sending"
-              />
-              <span class="md-error" v-if="!$v.form.lastName.required"
-                >The last name is required</span
-              >
-            </md-field>
-          </div>
-          <md-field :class="getValidationClass('email')">
-            <label for="email">Email</label>
+  <form novalidate @submit.prevent="validateUser">
+    <md-card>
+      <md-card-content>
+        <div class="md-small-size-50">
+          <md-field :class="getValidationClass('firstName')">
+            <label for="first-name">First Name</label>
             <md-input
-              type="email"
-              name="email"
-              id="email"
-              autocomplete="email"
-              v-model="form.email"
+              name="first-name"
+              id="first-name"
+              autocomplete="given-name"
+              v-model="form.firstName"
               :disabled="sending"
             />
-            <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
-            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+            <span class="md-error" v-if="!$v.form.firstName.required">
+              The first name is required
+            </span>
           </md-field>
-          <md-field :class="getValidationClass('password')">
-            <label for="password">Password</label>
+        </div>
+        <div class=" md-small-size-100">
+          <md-field :class="getValidationClass('lastName')">
+            <label for="last-name">last Name</label>
             <md-input
-              type="password"
-              name="password"
-              id="password"
-              autocomplete="password"
-              v-model="form.password"
+              name="last-name"
+              id="last-name"
+              autocomplete="family-name"
+              v-model="form.lastName"
               :disabled="sending"
             />
-            <span class="md-error" v-if="!$v.form.password.required">The password is required</span>
+            <span class="md-error" v-if="!$v.form.lastName.required">
+              The last name is required
+            </span>
           </md-field>
-        </md-card-content>
-        <md-card-actions>
-          <md-button type="submit" class="md-primary md-raised" :disabled="sending"
-            >Submit</md-button
-          >
-        </md-card-actions>
-      </md-card>
-    </form>
-  </div>
+        </div>
+        <md-field :class="getValidationClass('email')">
+          <label for="email">Email</label>
+          <md-input
+            type="email"
+            name="email"
+            id="email"
+            autocomplete="email"
+            v-model="form.email"
+            :disabled="sending"
+          />
+          <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+          <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+        </md-field>
+        <md-field :class="getValidationClass('password')">
+          <label for="password">Password</label>
+          <md-input
+            type="password"
+            name="password"
+            id="password"
+            autocomplete="password"
+            v-model="form.password"
+            :disabled="sending"
+          />
+          <span class="md-error" v-if="!$v.form.password.required">The password is required</span>
+        </md-field>
+      </md-card-content>
+      <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+      <md-card-actions>
+        <md-button type="submit" class="md-primary md-raised" :disabled="sending">
+          Submit
+        </md-button>
+      </md-card-actions>
+    </md-card>
+  </form>
 </template>
 <script>
 import { validationMixin } from 'vuelidate';
-import { required, email, minLength } from 'vuelidate/lib/validators';
+import { required, email } from 'vuelidate/lib/validators';
 export default {
   name: 'sign-up-form',
   mixins: [validationMixin],
+  props: {
+    onSubmit: {
+      type: Function,
+      required: true,
+    },
+    sending: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
   data: () => ({
     form: {
       firstName: null,
@@ -84,8 +92,6 @@ export default {
       password: null,
       email: null,
     },
-    userSaved: false,
-    sending: false,
   }),
   validations: {
     form: {
@@ -97,7 +103,6 @@ export default {
       },
       password: {
         required,
-        minLength: minLength(8),
       },
       email: {
         required,
@@ -128,11 +133,14 @@ export default {
       }
     },
   },
-  props: {
-    onSubmit: {
-      type: Function,
-      required: true,
-    },
-  },
 };
 </script>
+
+<style lang="scss" scoped>
+.md-progress-bar {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+}
+</style>
