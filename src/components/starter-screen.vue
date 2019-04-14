b39020713ca6468d8d1697c55f4e8c61e304c772
konstantin.ratushniak <konstantin.ratushniak@yola.com>
2019-04-14 15:19:57 +0300

    fix typo

--- src/components/starter-screen.vue
+++ src/components/starter-screen.vue
@@ -1,5 +1,5 @@
 <template>
-  <md-empty-state md-icon="chat" md-label="Wellcom To Chaty">
+  <md-empty-state md-icon="chat" md-label="Welcome To Chaty">
     <md-button class="md-primary md-raised" @click="onAddRoom">Create first room</md-button>
   </md-empty-state>
 </template>
